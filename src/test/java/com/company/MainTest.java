1aa649c44a4037e7f83c63f355eb56ff0b8c2fa6
vkama <krishna.amandu@gmail.com>
2026-02-17 17:22:58 +0530

    TestCase updated!!

--- src/test/java/com/company/MainTest.java
+++ src/test/java/com/company/MainTest.java
@@ -8,8 +8,8 @@ public class MainTest {
     @Test
     public void testHelloWorldOutput() {
         // This is a very basic test
-        String expected = "Hello, World!";
-        String actual = "Hello, World!"; // In real tests, you'd call a method
+        String expected = "Hello, World!!";
+        String actual = "Hello, World!!"; // In real tests, you'd call a method
         assertEquals(expected, actual);
     }
 }
