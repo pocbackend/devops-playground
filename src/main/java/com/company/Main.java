cfcb5b6e4d59d6ee3f0b72dc04034e756894d5ec
vkama <krishna.amandu@gmail.com>
2026-02-17 15:58:42 +0530

    Pipeline check log added!

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("**** Commit 1 ****");
         System.out.println("**** Commit 2 ****");
         System.out.println("**** Commit 3 ****");
+        System.out.println("**** Pipeline check ****");
     }
 }
